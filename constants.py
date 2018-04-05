3489be25935fec4514d1c9cdbef62d7989b287e6
ppgDev <sebastiaojosela@gmail.com>
2018-04-05 14:22:46 -0300

    code cleaning with pylint

--- constants.py
+++ constants.py
@@ -1,15 +1,15 @@
 import pygame
 
-#consts :
+# consts :
 #	COLORS
-RED = (255,000,000)
-GREEN = (000,255,000)
-BLUE = (000,000,255)
-WHITE = (255,255,255)
-BLACK = (000,000,000)
-BROWN = (139,69,19)
-YELLOW = (255,255,0)
-PINK = (248,24,148)
+RED = (255, 000, 000)
+GREEN = (000, 255, 000)
+BLUE = (000, 000, 255)
+WHITE = (255, 255, 255)
+BLACK = (000, 000, 000)
+BROWN = (139, 69, 19)
+YELLOW = (255, 255, 0)
+PINK = (248, 24, 148)
 
 # 	TYPES
 ORGANICO = 0
@@ -19,13 +19,13 @@ PLASTICO = 3
 VIDRO = 4
 # SCREEN SIZE
 
-# pygame.display.init()
-# screen = pygame.display.Info()
-# WIDTH = int(screen.current_w*0.9)
-# HEIGHT = int(screen.current_h*0.9)
+pygame.display.init()
+SCREEN = pygame.display.Info()
+WIDTH = int(SCREEN.current_w*0.9)
+HEIGHT = int(SCREEN.current_h*0.9)
 
 
-WIDTH = 800
-HEIGHT = 600
+# WIDTH = 800
+# HEIGHT = 600
 
-FPS = 30
+FPS = 60
